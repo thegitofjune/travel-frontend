ca7aa1076e84e02047346a732cdd865fc350d2e9
June <june.mcpake@gmail.com>
2023-03-02 09:54:14 +0000

    adds the rating attribute to the add location form

--- src/components/AddLocation.jsx
+++ src/components/AddLocation.jsx
@@ -9,6 +9,7 @@ const AddLocation = () => {
     const locationApi = `http://localhost:8088/api/v1/locations/addLocation`
     const [name, setName] = useState('')
     const [review, setReview] = useState('')
+    const [rating, setRating] = useState("")
     const sleep = ms => new Promise(r => setTimeout(r, 1000));
 
     const createLocation = (event) => {
@@ -18,6 +19,7 @@ const AddLocation = () => {
         axios.post(locationApi, {
             name: name,
             review: review,
+            rating: rating
         }).then(() => {
             sleep();
             navigate('/locations')
@@ -34,6 +36,10 @@ const AddLocation = () => {
                 <div className="form-group">
                     <label htmlFor="exampleInputReview">Review:</label>
                     <textarea className="form-control" id="review" rows="5" placeholder="Enter location review" value={review} onChange={event => setReview(event.target.value)}></textarea>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputRating">Rating4:</label>
+                    <input type="number" className="form-control" id="review"  placeholder="Add a rating from 1 to 5" value={rating} onChange={event => setRating(event.target.value)}/>
                 </div>
                 <button type="submit" className="btn btn btn-success  button-padding">Submit</button>
                 <button type="button" className="btn btn-danger button-padding" onClick={() => navigate('/locations')}>Cancel</button>
